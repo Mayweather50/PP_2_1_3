65f8dbe4e5311e0d92ddd71f4a68fbb4fcaab661
Mayweather50 <62692570+Mayweather50@users.noreply.github.com>
2023-10-18 22:08:07 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+
+        Cat catbean = (Cat) applicationContext.getBean("cat");
+
+
+
+        Cat catbean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catbean == catbean2);
     }
 }
